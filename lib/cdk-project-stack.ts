a48c3c15652c9c54f254719a0778cfc12c948a3e
Erik <erikchaulk@gmail.com>
2021-10-06 19:59:43 -0400

    API with query string parameters

--- lib/cdk-project-stack.ts
+++ lib/cdk-project-stack.ts
@@ -2,6 +2,8 @@ import * as cdk from '@aws-cdk/core';
 import * as apigateway from '@aws-cdk/aws-apigateway';
 import * as lambda from '@aws-cdk/aws-lambda';
 import * as dynamodb from '@aws-cdk/aws-dynamodb';
+import { LambdaIntegration } from '@aws-cdk/aws-apigateway';
+import { request } from 'http';
 
 export class CdkProjectStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
@@ -9,29 +11,67 @@ export class CdkProjectStack extends cdk.Stack {
 
     //DynamoDB Table Definition
     const table = new dynamodb.Table(this, 'aws-cdk-dynamodb-table', {
-      partitionKey: { name: 'id', type: dynamodb.AttributeType.STRING }
+      partitionKey: { name: 'gameName', type: dynamodb.AttributeType.STRING }
     });
 
     //Lambda Function
     const lambdaFunction = new lambda.Function(this, 'aws-cdk-lambda-function', {
       code: lambda.Code.fromAsset("functions"),
       handler: 'index.handler',
-      runtime: lambda.Runtime.NODEJS_12_X
+      runtime: lambda.Runtime.NODEJS_12_X,
+      environment: {
+        DYNAMO_DB_TABLE: table.tableName
+      }
     });
 
     table.grantReadWriteData(lambdaFunction);   
-
+    
     //API Gateway and methods
     const api = new apigateway.LambdaRestApi(this, "aws-cdk-rest-api", {
       handler: lambdaFunction,
-      proxy: false
+      proxy: false,
+      restApiName: "Game Management API"
+    });
+    const apiIntegration = new apigateway.LambdaIntegration(lambdaFunction);
+    const requestValidator = api.addRequestValidator("request-validator", {
+      requestValidatorName: "queryStringValidator",
+      validateRequestBody: false,
+      validateRequestParameters: true
     });
 
-    api.root.addResource("getGame").addMethod("GET");
-    api.root.addResource("listGames").addMethod("GET");
-    api.root.addResource("createGame").addMethod("POST");    
-    api.root.addResource("modifyGame").addMethod("PUT");    
-    api.root.addResource("deleteGame").addMethod("DELETE"); 
+    api.root.addResource("getGame").addMethod("GET", apiIntegration, {
+      requestParameters: {
+        "method.request.querystring.gameName": true,
+      },
+      requestValidator: requestValidator,
+    });
+    api.root.addResource("listGames").addMethod("GET", apiIntegration);
+    api.root.addResource("createGame").addMethod("POST", apiIntegration, {
+      requestParameters: {
+        "method.request.querystring.gameName": true,
+        "method.request.querystring.genre": false,
+        "method.request.querystring.description": false,
+        "method.request.querystring.console": false,
+        "method.request.querystring.yearReleased": false
+      },
+      requestValidator: requestValidator,
+    });    
+    api.root.addResource("modifyGame").addMethod("PUT", apiIntegration, {
+      requestParameters: {
+        "method.request.querystring.gameName": true,
+        "method.request.querystring.genre": false,
+        "method.request.querystring.description": false,
+        "method.request.querystring.console": false,
+        "method.request.querystring.yearReleased": false
+      },
+      requestValidator: requestValidator,
+    });   
+    api.root.addResource("deleteGame").addMethod("DELETE", apiIntegration, {
+      requestParameters: {
+        "method.request.querystring.gameName": true
+      },
+      requestValidator: requestValidator
+    });
 
   }
 }
